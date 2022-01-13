71777ae1c505fd5019806299ad1ee01d133da05a
leilaFideMorvari <96225603+leilaFideMorvari@users.noreply.github.com>
2022-01-12 18:15:27 -0800

    Add files via upload

---
+++ HobbyBook_Unsolved.py
@@ -0,0 +1,16 @@
+# @TODO: Your code here
+dog_info = {
+    "name": "Puppy",
+    "age" : 9,
+    "hobbies" : ["walking,", "Fetch", "Barking"],
+    "wakeup" : {"Mon":"7am", "Tue":"10am"}
+
+}
+
+
+
+print(f"My dog's name is {dog_info['name']}")
+
+print(dog_info["wakeup"]["Tue"])
+print(dog_info["hobbies"][1])
+
